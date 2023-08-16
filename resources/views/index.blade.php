60f87548288915915be8026480ec1ccde208ebc7
Bagos Hadi Wijaya <bagoshadi123@gmail.com>
2023-08-16 16:46:05 +0700

    updateHomePage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
         <div class="container">
           <a class="navbar-brand" href="#">
             <img src="{{ asset('frontend/assets/image/logo-70.png') }}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top me-3">
-            Rumah Impian
+            Pijat.in
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -59,18 +59,64 @@
         <div class="container h-100">
             <div class="row h-100">
                 <div class="col-md-6 hero-tagline my-auto">
-                    <h1>Membantu Orang Tua</h1>
-                    <p><span class="fw-bold">Rumah Impian</span> hadir untuk temukan rumah terbaik untukmu, untuk di jual ataupun di sewa dengan sumber terpercaya.</p>
+                    <h1>Santai tubuh dan pikiran, biarkan pijatan mengalirkan kebahagiaan </h1>
+                    <p><span class="fw-bold">Nikmati momen istimewa</span> untuk meremajakan tubuh dan pikiran di Oasis Pijat Sehat dan Relaksasi. Kami memiliki tim terlatih dan berpengalaman dalam memberikan layanan pijat berkualitas yang membantu Anda meraih ketenangan dan kesejahteraan.</p>
 
                     <button class="button-lg-primary">Temukan Rumah</button>
                     <a href="#"><img src="{{ asset('frontend/assets/image/arrow.png') }}" alt=""></a>
                 </div>
             </div>
-            <img src="{{ asset('frontend/assets/image/hero_banner.png') }}" alt="" class="position-absolute end-0 bottom-0 img-hero">
+            <img src="{{ asset('frontend/assets/image/pijit.png') }}" alt="" class="position-absolute end-0 bottom-0 img-hero">
             <img src="{{ asset('frontend/assets/image/accent.png') }}" alt="" class="accent-img h-100 position-absolute top-0 start-0">
         </div>
       </section>
 
+      <!-- Layanan Section -->
+      <section id="layanan">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 text-center">
+                <h2>Layanan Kami</h2>
+                <span class="sub-title">Rumah impian hadir menjadi solusi bagi kamu</span>
+            </div>
+          </div>
+
+          <div class="row mt-5">
+            <div class="col-md-4 text-center">
+              <div class="card-layanan">
+                <div class="circle-icon position-relative mx-auto">
+                  <img src="{{ asset('frontend/assets/image/house 1.png') }}" alt="" class="position-absolute top-50 start-50 translate-middle">
+                </div>
+                 <h3 class="mt-4">Property Baru</h3>
+              <p class="mt-3">Rumah impian kini jadi kenyataan, Beli rumah bary dengan fasilitas terbaik
+                 dengan lingkungan yang nyaman.</p>
+              </div>
+            </div>
+
+            <div class="col-md-4 text-center">
+              <div class="card-layanan">
+                <div class="circle-icon position-relative mx-auto">
+                  <img src="{{ asset('frontend/assets/image/house 1.png') }}" alt="" class="position-absolute top-50 start-50 translate-middle">
+                </div>
+                 <h3 class="mt-4">Property Baru</h3>
+              <p class="mt-3">Rumah impian kini jadi kenyataan, Beli rumah bary dengan fasilitas terbaik
+                 dengan lingkungan yang nyaman.</p>
+              </div>
+            </div>
+
+            <div class="col-md-4 text-center">
+              <div class="card-layanan">
+                <div class="circle-icon position-relative mx-auto">
+                  <img src="{{ asset('frontend/assets/image/house 1.png') }}" alt="" class="position-absolute top-50 start-50 translate-middle">
+                </div>
+                 <h3 class="mt-4">Property Baru</h3>
+              <p class="mt-3">Rumah impian kini jadi kenyataan, Beli rumah bary dengan fasilitas terbaik
+                 dengan lingkungan yang nyaman.</p>
+              </div>
+            </div>
+          </div>
+        </div> 
+      </section>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
