6ef1b5ce7262e8d3193a50fec9c3afed1189870a
bayupras141 <aebayu789@gmail.com>
2023-08-23 20:41:18 +0700

    configure model superAdmin

--- app/Models/SuperAdmin.php
+++ app/Models/SuperAdmin.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class SuperAdmin extends Model
 {
     use HasFactory;
+    use HasFactory;
+    protected $table = 'super_admins';
+    protected $fillable = ['jenis_kelamin', 'alamat', 'no_hp', 'foto', 'ktp', 'user_id', 'status', 'tempat_lahir', 'tanggal_lahir', 'ktp'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
