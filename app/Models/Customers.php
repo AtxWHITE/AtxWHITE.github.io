05d25625652d4147d8db04ed644ad9505fc6a80b
bayupras141 <aebayu789@gmail.com>
2023-08-25 14:14:19 +0700

    fix codingan bertumpuk

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -12,14 +12,14 @@ class Customers extends Model
 
     // table customers
     protected $table = 'customers';
+    protected $fillable = ['jenis_kelamin', 'alamat', 'no_hp', 'foto', 'ktp', 'user_id', 'status', 'tempat_lahir', 'tanggal_lahir', 'ktp'];
 
     // relasi dengan order
     public function order()
     {
         return $this->hasMany(Order::class);
+    }
 
-    protected $table = 'customers';
-    protected $fillable = ['jenis_kelamin', 'alamat', 'no_hp', 'foto', 'ktp', 'user_id', 'status', 'tempat_lahir', 'tanggal_lahir', 'ktp'];
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
