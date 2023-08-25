4eb2dfa2139f85179b36b0f747164aa49008e4b9
bayupras141 <aebayu789@gmail.com>
2023-08-25 13:50:25 +0700

    relasi customer to order

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     use HasFactory;
+
+    // table customers
+    protected $table = 'customers';
+
+    // relasi dengan order
+    public function order()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
