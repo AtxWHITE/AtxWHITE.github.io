6949cc95b8c19560f660832cd3f72a4d261e9302
bayupras141 <aebayu789@gmail.com>
2023-08-28 06:41:03 +0700

    model admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $table = 'admins';
+    protected $fillable = ['jenis_kelamin', 'alamat', 'no_hp', 'foto', 'ktp', 'user_id', 'status', 'tempat_lahir', 'tanggal_lahir', 'ktp'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
